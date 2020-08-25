6b106696f4634ceaf748ed42728b6d8ced8e8898
Keith <ko.keith@hotmail.com>
2020-08-25 15:39:32 -0400

    add semantic ui

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 
 Uses Actioncable to implement real time messaging.
+
+* Rails Version: 6.0.3.2
+
+* Ruby Version: ruby 2.6.6p146
+
+* Front-end: Semantic UI 2.4.2
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'chatroom/index' => "chatroom#index"
+  root "chatroom#index"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "session#destroy"
+
 end
