c499f3db3aa4c90eb145e86bd2b2c38b8444cda4
Keith <ko.keith@hotmail.com>
2020-08-26 16:16:58 -0400

    add helper methods and sessions_controller functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,23 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            # good scenario
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to root_path
+        else
+            #either user invalid or authentication failed
+            flash.now[:error] = "There was something wrong with your login information"
+            render 'new'
+        end
     end
 
     def destroy 
-
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to login_path
     end
 
 
